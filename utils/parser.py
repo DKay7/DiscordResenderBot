411ce539d6a7409fa481ecf680d96269bd7c10e4
Danny <bublikplushka@yandex.ru>
2021-07-06 23:09:04 +0300

    Added parser exception handling

--- utils/parser.py
+++ utils/parser.py
@@ -25,7 +25,8 @@ def read_file_to_df(content: str):
     :param content: string object with content which will be parsed to dataframe object
     """
     content_io = StringIO(content)
-    dataframe = pd.read_csv(content_io, encoding="cp1251", index_col=False, header=None)
+    dataframe = pd.read_csv(content_io, encoding="cp1251", index_col=False, header=None,
+                            error_bad_lines=False)
 
     return dataframe
 
@@ -37,6 +38,7 @@ def parse_file_content(content):
     :param content: string object with content which will be parsed
     """
     dataframe = read_file_to_df(content)
+    dataframe = dataframe.dropna(how='all')
 
     datetimes = []
     chat_types = []
@@ -46,14 +48,17 @@ def parse_file_content(content):
     authors = []
 
     for row in dataframe.index:
+        try:
+            date_and_time = dt.strptime(dataframe[0][row], "%m/%d/%Y %H:%M:%S.%f")
+            datetimes.append(TIMEZONE.localize(date_and_time))
 
-        date_and_time = dt.strptime(dataframe[0][row], "%m/%d/%Y %H:%M:%S.%f")
-        datetimes.append(TIMEZONE.localize(date_and_time))
+            chat_types.append(dataframe[1][row])
+            coordinates.append((dataframe[4][row], dataframe[5][row]))
+            messages.append(dataframe[7][row])
+            authors.append(dataframe[8][row])
 
-        chat_types.append(dataframe[1][row])
-        coordinates.append((dataframe[4][row], dataframe[5][row]))
-        messages.append(dataframe[7][row])
-        authors.append(dataframe[8][row])
+        except (ValueError, TypeError, UnicodeError):
+            continue
 
     return {"datetime": datetimes,
             "chat_types": chat_types,
